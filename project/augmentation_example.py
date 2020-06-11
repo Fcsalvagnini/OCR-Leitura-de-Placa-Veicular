5fb8f602f5ef2fa8e91f0b193e36ce7943adf8c1
Fcsalvagnini <felip.eng@outlook.com.br>
2020-06-11 17:35:40 -0300

    Adicionado script para agumentacao das imagens utilizando a biblioteca albumentations, tambem foi implementada uma funcao para efetuar a leitura do dataset em hdf5

--- project/augmentation_example.py
+++ project/augmentation_example.py
@@ -1,36 +1,42 @@
 import h5py
 import cv2
-from albumentations import Rotate
+import numpy as np
+from albumentations import (Compose, Rotate, Blur, RandomBrightnessContrast, IAAPerspective, RandomScale)
 
-hdf5_file_path = "characters_dataset.hdf5"
+def read_hdf5_dataset(path_to_hdf5):
+    hdf5_dataset = h5py.File(path_to_hdf5, "r")
+    characters_images = hdf5_dataset["characters_images"]
+    characters_labels = hdf5_dataset["labels"]
+    
+    return hdf5_dataset, (characters_images, characters_labels)
 
 def apply_image_augmentation(augmentator, image):
     augmented_image = augmentator(image = image)["image"]
     return augmented_image
 
+def horizontal_stack_images(image_1, image_2):
+    return np.hstack((image_1, image_2))
+
 if __name__ == "__main__":
-    # Instancia o objeto de augmentação
-    augmentator = Rotate(15, p = 0.5)
-
     print("[INFO] Augmentation example, press 'q' to finish the example")
-    with h5py.File("characters_dataset.hdf5", "r") as hdf5_file:
-        characters_images = hdf5_file["characters_images"]
-        characters_labels = hdf5_file["labels"]
+    # Instancia o objeto de augmentação
+    augmentator = Compose(  [Rotate(15), Blur(5), IAAPerspective(scale = (0.025, 0.05), p = 0.8),
+                            RandomBrightnessContrast(brightness_limit = 0.35, contrast_limit = 0.35)], 
+                            p = 0.7 )
+
+    dataset, (characters_images, characters_labels) = read_hdf5_dataset("characters_dataset.hdf5")
 
 
+    for idx, image in enumerate(characters_images):
+        augmented_image = apply_image_augmentation(augmentator, image)
+        cv2.imshow("Character x Augmented character", horizontal_stack_images(image, augmented_image))
+        print("[INFO] Label => ", characters_labels[idx])
+        
+        key = cv2.waitKey(0) & 0xff
+        if key == ord('q'):
+            break
 
-        for idx, image in enumerate(characters_images):
-            augmented_image = apply_image_augmentation(augmentator, image)
-            print(augmented_image)
-            cv2.imshow("Character", image)
-            cv2.imshow("Augmented character", augmented_image)
-            print("[INFO] Label => ", characters_labels[idx])
-            
-            key = cv2.waitKey(0) & 0xff
-            if key == ord('q'):
-                break
-
-
+    dataset.close()
     print("[INFO] Augmentation example finished")
 
     
--- project/reading_hdf5_file.py
+++ project/reading_hdf5_file.py
@@ -3,18 +3,25 @@ import cv2
 
 hdf5_file_path = "characters_dataset.hdf5"
 
+def read_hdf5_dataset(path_to_hdf5):
+    hdf5_dataset = h5py.File(path_to_hdf5, "r")
+    characters_images = hdf5_dataset["characters_images"]
+    characters_labels = hdf5_dataset["labels"]
+    
+    return hdf5_dataset, (characters_images, characters_labels)
+
 if __name__ == "__main__":
     print("[INFO] Reading example, press 'q' to finish the example")
-    with h5py.File("characters_dataset.hdf5", "r") as hdf5_file:
-        characters_images = hdf5_file["characters_images"]
-        characters_labels = hdf5_file["labels"]
 
-        for idx, image in enumerate(characters_images):
-            cv2.imshow("Character", image)
-            print("[INFO] Label => ", characters_labels[idx])
-            
-            key = cv2.waitKey(0) & 0xff
-            if key == ord('q'):
-                break
+    dataset, (characters_images, characters_labels) = read_hdf5_dataset("characters_dataset.hdf5")
 
+    for idx, image in enumerate(characters_images):
+        cv2.imshow("Character", image)
+        print("[INFO] Label => ", characters_labels[idx])
+        
+        key = cv2.waitKey(0) & 0xff
+        if key == ord('q'):
+            break
+
+    dataset.close()
     print("[INFO] Reading example finished")
